540b096b8a00a71a00388dd90b9978069da7cc1d
hakyung <hakyung.dev@gmail.com>
2020-11-20 15:55:51 +0900

    Move currentLocation into userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,13 +2,20 @@ import * as types from '../constants/actionTypes';
 
 const initialState = {
   name: null,
+  currentLocation: { lat: 37.572823, lng: 126.976881 },
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.SET_NAME:
       return {
+        ...state,
         name: action.name,
+      };
+    case types.GET_CURRENT_LOCATION:
+      return {
+        ...state,
+        currentLocation: action.location,
       };
     default:
       return state;
