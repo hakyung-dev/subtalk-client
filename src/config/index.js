be65f05ac5b7c8bbfea7073bdd4d96bc04dad57d
hakyung <hakyung.dev@gmail.com>
2020-12-04 01:46:24 +0900

    Change SERVER_URL and head title

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,6 @@
-const SERVER_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'PRODUCT';
+const SERVER_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001'
+    : 'https://subtalkapi.hakyung.site';
 
 module.exports = { SERVER_URL };
