09ed417a197019a8d0225d27f6f60749b1f36c46
hakyung <hakyung.dev@gmail.com>
2020-12-04 00:45:08 +0900

    Add Timer for redirection to index page

--- src/components/OutModal.js
+++ src/components/OutModal.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import train from '../styles/images/train.png';
+
+import Timer from './Timer';
 
 const OutModal = (props) => {
   const { closeModal } = props;
 
+  useEffect(() => {
+    const callOut = setTimeout(closeModal, 11000);
+    return () => clearTimeout(callOut);
+  }, [closeModal]);
+
   return (
-    <div className="out" onClick={() => closeModal()}>
+    <div className="out">
       <div className="container-alarm">
         <div className="modal-body">
           <div className="title-modal">ERROR</div>
-          <div className="alarm">열차와 사용자의 위치가 벗어났습니다.</div>
+          <div className="alarm">
+            열차와 사용자의 위치가 벗어났습니다.<br></br>
+            홈으로 이동하기 <Timer leftTime={10} />
+          </div>
           <div className="container-train">
             <img src={train} className="train" alt="train" />
           </div>
