04628dab7b1f47abc8845330fd17fdc807782308
hakyung <hakyung.dev@gmail.com>
2021-05-19 14:34:30 +0900

    Renew server url

--- src/config/socket.js
+++ src/config/socket.js
@@ -3,6 +3,6 @@ import io from 'socket.io-client';
 const socketClient =
   process.env.NODE_ENV === 'development'
     ? io('http://localhost:3001')
-    : io('https://subtalkapi.hakyung.site');
+    : io('https://subtalkapi.devhak.xyz');
 
 export default socketClient;
